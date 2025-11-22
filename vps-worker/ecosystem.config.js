fc0a986d5c25556810800f1c1d545edee533f492
anwesh-personal <anwesh@creaova.com>
2025-11-22 12:14:53 +0530

    2ND

--- vps-worker/ecosystem.config.js
+++ vps-worker/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
     error_file: '/home/lekhika.online/vps-worker/logs/lekhika-worker-error.log',
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
     merge_logs: true,
-    max_memory_restart: '1G',
+    max_memory_restart: '3G', // Increased from 1G to prevent restarts during long-running executions
     restart_delay: 4000,
     max_restarts: 10,
     min_uptime: '10s',
